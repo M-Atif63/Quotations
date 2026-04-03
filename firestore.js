c43a3f6e814565c8b25d32e3e23213e1d19864bb
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-04-03 13:22:38 +0500

    feature:added edit button and delete button with the help of js

--- firestore.js
+++ firestore.js
@@ -40,3 +40,39 @@ async function setQuote() {
     })
     getQoute()
 }
+async function getQoute() {
+    quoteList.innerHTML = ""
+    const querySnapshot = await getDocs(dataColl);
+    querySnapshot.forEach((doc) => {
+        var li = document.createElement("li")
+        li.textContent = `${doc.data().quote}`
+        var editBtn = document.createElement("button")
+        editBtn.textContent = "Edit"
+        editBtn.addEventListener("click", function () {
+            editQoute(doc.id, doc.data().quote)
+        })
+        var deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
+        deleteBtn.addEventListener("click", function () {
+            deleteQoute(doc.id)
+        })
+        li.appendChild(editBtn)
+        li.appendChild(deleteBtn)
+        quoteList.appendChild(li)
+    });
+}
+getQoute()
+
+async function editQoute(id, oldQuote) {
+    var newQuote = prompt("enter new quote", oldQuote)
+    await updateDoc(doc(database, "quotes", id), {
+        quote: newQuote
+    })
+}
+getQoute()
+
+
+async function deleteQoute(id) {
+    await deleteDoc(doc(database, "quotes", id))
+    getQoute()
+}
